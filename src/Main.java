f67658fa6f3b45a2fffda24895452cd8f57d998b
jinzhihang <191041707@qq.com>
2017-02-16 21:06:52 +0800

    第3次尝试

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello C!");
         System.out.println("这是直接由本地上传到github上的");
         System.out.println("第二次尝试");
+        System.out.println("第三次尝试");
     }
 }
