b1c0ac6190a190b00083a142be1a8317c54c281d
jinzhihang <191041707@qq.com>
2017-02-16 21:02:48 +0800

    第一次尝试

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello C!");
         System.out.println("这是直接由本地上传到github上的");
+        System.out.println("第二次尝试");
     }
 }
